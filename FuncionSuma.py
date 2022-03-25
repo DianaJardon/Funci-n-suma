67eaac9c7ec58a9cf5acc0a6e292d8f9fe6664ce
Diana Jardon <ada_wit29@hackademy.mx>
2022-03-24 23:01:40 -0600

    Firts commit

---
+++ FuncionSuma.py
@@ -0,0 +1,13 @@
+def suma(x,y):
+    z = x+y
+    print(f'La suma {x} + {y} = {z}')
+suma(3,2)
+
+def suma():
+    x = int(input("Ingresa un valor para x: "))
+    y = int(input("Ingresa un valor para y: "))
+    z = x+y
+    
+    print(f'La suma {x} + {y} = {z}')
+
+suma()
